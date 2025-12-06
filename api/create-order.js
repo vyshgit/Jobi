62bf185d78c63f4fdb751d9487a27501f75d1b35
vyshgit <vyshcodw@gmail.com>
2025-12-06 19:13:20 +0530

    upi only pg

--- api/create-order.js
+++ api/create-order.js
@@ -15,6 +15,7 @@ export default async function handler(req, res) {
       amount: amount * 100,
       currency: "INR",
       receipt: orderId,
+      checkout_config_id: "config_RoLAkKg0Awnnav"
     });
 
     return res.json({
